8f0b75e7928862e1abdc1444f14b6f5a3c00f3eb
Alex Good <alexjsgood@gmail.com>
2012-08-21 15:52:40 +0100

    Bumping version in GoCardless.java

--- src/main/java/gocardless/GoCardless.java
+++ src/main/java/gocardless/GoCardless.java
@@ -10,33 +10,33 @@ import gocardless.webhook.WebHook;
  *
  */
 public class GoCardless {
-  
-  public final static String VERSION = "1.0.1";
-  
+
+  public final static String VERSION = "1.1.0";
+
   public final static AccountDetails accountDetails = new AccountDetails();
-  
+
   public final static Api api = new Api(accountDetails);
-  
+
   public final static Connect connect = new Connect(accountDetails);
-  
+
   public final static Partner partner = new Partner(accountDetails);
-  
+
   public final static WebHook webHook = new WebHook(accountDetails);
-  
+
   public interface ApiBase {
-    public static final String PRODUCTION = "https://gocardless.com";  
+    public static final String PRODUCTION = "https://gocardless.com";
     public static final String SANDBOX = "https://sandbox.gocardless.com";
   }
-  
+
   public enum Environment {PRODUCTION, SANDBOX};
-  
+
   public static Environment environment = Environment.PRODUCTION;
-  
+
   protected GoCardless() {
   }
-  
+
   public static String getApiBase() {
     return (Environment.SANDBOX.equals(environment)) ? ApiBase.SANDBOX : ApiBase.PRODUCTION;
   }
-  
+
 }
