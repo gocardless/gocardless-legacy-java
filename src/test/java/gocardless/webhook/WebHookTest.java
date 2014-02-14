eab521a625a572b9a5d825b1cea3ef2f7cc69d29
willedwards <will.edwards@me.com>
2014-02-14 13:02:26 +0000

    better way to store json

--- src/test/java/gocardless/webhook/WebHookTest.java
+++ src/test/java/gocardless/webhook/WebHookTest.java
@@ -1,9 +1,12 @@
 package gocardless.webhook;
 
 import gocardless.AccountDetails;
-
 import gocardless.exception.SignatureException;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class WebHookTest {
   
@@ -11,10 +14,30 @@ public class WebHookTest {
     .appId("id01").appSecret("sec01").accessToken("tok01").merchantId("mer01").build();
   
   private WebHook webHook = new WebHook(accountDetails);
-  
+  private String webhookJsonData;
+
+  @Before
+  public void init() throws IOException {
+      String rawwebhookJsonData = readFromRawResourceFile("/webhook.json");
+      webhookJsonData = removeNewLines(rawwebhookJsonData);
+  }
+
   @Test
   public void testValidate() throws SignatureException {
-    webHook.validate(Fixtures.WEBHOOK);
+    webHook.validate(webhookJsonData);
   }
-  
+
+    private String removeNewLines(String input)
+    {
+        return input.replaceAll("\n","").replaceAll("\\s+$", "");
+    }
+
+  private String readFromRawResourceFile(String filename) throws IOException
+  {
+        InputStream is = getClass().getResourceAsStream(filename);
+        byte[] buff = new byte[2000];
+        is.read(buff);
+        byte[] results = new String(buff).trim().getBytes();
+        return new String(results);
+  }
 }
